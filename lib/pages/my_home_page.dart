8202bd11b8f9c90058e77f7a0ad2c2b092e7ac54
Thakshila Fonseka <thakshilafonseka2002@gmail.com>
2025-03-29 08:48:30 +0530

    Commiting Changes

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -10,6 +10,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: SafeArea(child: Scaffold(
+
+      )),
+    );
   }
 }
