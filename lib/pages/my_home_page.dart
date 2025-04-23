752c2615c2da2e2e752483c63189161685c0462f
Thakshila Fonseka <thakshilafonseka2002@gmail.com>
2025-04-23 21:57:54 +0530

    Commiting Changes

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ebook_app_readify/audio/app_colors.dart' as AppColors;
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -11,10 +12,30 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: SafeArea(child: Scaffold(
-        appBar: AppBar(),
-
-      )),
+      color: AppColors.background,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(),
+          body: Column(
+            children: [
+              Row(
+                children: [
+                  ImageIcon(
+                    AssetImage("assets/images/menu.jpg"),
+                    size: 24,
+                    color: Colors.black,
+                  ),
+                  Row(
+                    children: [
+                      //ImageIcon(AssetImage(""))
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
